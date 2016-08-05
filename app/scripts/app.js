2833b0592e015814e7a3479691e3793fb5fc97fe
Tran Nguyen <baotranmimi@gmail.com>
2016-08-05 05:14:38 +0000

    Finish Groceries sample (Add data and Authentication) Changes made by Tran Nguyen (baotranmimi@gmail.com).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,28 +1,151 @@
-(function() {
-    var apiKey = "YOUR API KEY";
+(function () {
+    var apiKey = "rvmvkcl4sbya4fs4";
     var el = new Everlive(apiKey);
 
+    // var groceryDataSource = new kendo.data.DataSource({
+    //     transport: {
+    //         read: {
+    //             url: "https://api.everlive.com/v1/" + apiKey + "/Groceries",
+    //             dataType: "json"
+    //         }
+    //     },
+    //     schema: {
+    //         data: function (response) {
+    //             return response.Result;
+    //         }
+    //     }
+    // });
+
     var groceryDataSource = new kendo.data.DataSource({
-        transport: {
-            read: {
-                url: "https://api.everlive.com/v1/" + apiKey + "/Groceries",
-                dataType: "json"
-            }
+        type: "everlive",
+        sort: {
+            field: "Name",
+            dir: "asc"
         },
-        schema: {
-            data: function(response) {
-                return response.Result;
-            }
+        transport: {
+            typeName: "Groceries"
         }
     });
 
     function initialize() {
+        // el.Users.login("Mimi", "Mimi",
+        //     function (data) {
+        //         groceryDataSource.read();
+        //     }
+        // );
         var app = new kendo.mobile.Application(document.body, {
             skin: "nova",
             transition: "slide"
         });
+
+        $("#grocery-list").kendoMobileListView({
+            dataSource: groceryDataSource,
+            template: "#: Name #"
+        });
+
         navigator.splashscreen.hide();
     }
+
+    window.loginView = kendo.observable({
+        submit: function () {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.login(this.username, this.password,
+                function (data) {
+                    window.location.href = "#list";
+                    groceryDataSource.read();
+                },
+                function () {
+                    navigator.notification.alert("Unfortunately we could not find your account.");
+                });
+        }
+    });
+
+    window.registerView = kendo.observable({
+        submit: function () {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            el.Users.register(this.username, this.password, {
+                    Email: this.email
+                },
+                function () {
+                    navigator.notification.alert("Your account was successfully created.");
+                    window.location.href = "#login";
+                },
+                function () {
+                    navigator.notification.alert("Unfortunately we were unable to create your account.");
+                });
+        }
+    });
+
+    window.passwordView = kendo.observable({
+        submit: function () {
+            if (!this.email) {
+                navigator.notification.alert("Email address is required.");
+                return;
+            }
+            $.ajax({
+                type: "POST",
+                url: "https://api.everlive.com/v1/" + apiKey + "/Users/resetpassword",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    Email: this.email
+                }),
+                success: function () {
+                    navigator.notification.alert("Your password was successfully reset. Please check your email for instructions on choosing a new password.");
+                    window.location.href = "#login";
+                },
+                error: function () {
+                    navigator.notification.alert("Unfortunately, an error occurred resetting your password.")
+                }
+            });
+        }
+    });
+
+    window.listView = kendo.observable({
+        logout: function (event) {
+            // Prevent going to the login page until the login call processes.
+            event.preventDefault();
+            el.Users.logout(function () {
+                this.loginView.set("username", "");
+                this.loginView.set("password", "");
+                window.location.href = "#login";
+            }, function () {
+                navigator.notification.alert("Unfortunately an error occurred logging out of your account.");
+            });
+        }
+    });
+
+    window.addView = kendo.observable({
+        add: function () {
+            if (!this.grocery) {
+                navigator.notification.alert("Please provide a grocery.");
+                return;
+            }
+
+            groceryDataSource.add({
+                Name: this.grocery
+            });
+            groceryDataSource.one("sync", this.close);
+            groceryDataSource.sync();
+            this.set("grocery", "");
+        },
+        close: function () {
+            $("#add").data("kendoMobileModalView").close();
+        }
+    });
 
     document.addEventListener("deviceready", initialize);
 }());
